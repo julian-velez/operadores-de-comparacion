cd150ba14532224047e690c20a57ea7c6b951925
julian-velez <60607869+julian-velez@users.noreply.github.com>
2022-11-01 11:19:51 -0500

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+let nombre01 = "julian";
+let nombre02 = "marisol";
+
+console.log(nombre01 == nombre02);
+
+let edad01 = 15;
+let edad02 = 40;
+
+console.log(edad01 < edad02 );
+
+let peso = "15";
+let medida = "kg";
+
+console.log(peso +  '10');
+
+console.log([] == 0);
